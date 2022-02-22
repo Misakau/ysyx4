0c598ea46e013b13da815c9f6ec1aa24219a8bc4
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-22 22:33:59 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:33:59 up 48 min,  1 user,  load average: 0.29, 0.19, 0.21

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -275,7 +275,6 @@ word_t eval(int p,int q,bool *succ){
 	  word_t val=0;
 	  bool ff=true;
 	  val=isa_reg_str2val(tokens[p].str,&ff);
-	  printf("%s: %lu, %d\n",tokens[p].str,val,ff);
 	  if(ff) return val;
 	  else{
 		*succ=false;
