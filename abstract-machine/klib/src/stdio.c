6a36867da6cc28877059915d74b325b3b2fc2349
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-26 18:33:30 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:33:30 up 17 min,  1 user,  load average: 0.31, 0.36, 0.26

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,48 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  assert(out!=NULL);
+  char* po=out;const char* pf=fmt;char* ps;
+  int tot=0;int k;char cc[128];int tmp=0;
+  va_list vlist;
+  va_start(vlist,fmt);
+  
+  while(*pf!='\0'){
+    if(*pf!='%') { *po=*pf; tot++;po++;}
+    else{
+      pf++; assert(*pf!='\0');
+      switch(*pf){
+	case 's':
+	  ps=va_arg(vlist,char*);
+	  while(*ps!='\0'){
+	    *po++=*ps++;
+	    tot++;
+	  }
+	  break;
+        case 'd':
+          tmp=0;
+	  k=va_arg(vlist,int);
+	  if(k==0){
+	    *po='0';tot++;po++;break;
+	  }
+	  if(k<0){*po='-';tot++;po++;k=-k;}
+	  while(k>0){
+	    cc[tmp++]=k%10+'0';k/=10;
+	  }
+	  for(int i=tmp-1;i>=0;i--){*po=cc[i];po++;tot++;}
+	  break;
+	default : assert(0);
+      }
+    }
+    pf++;
+  }
+  *po='\0';
+  
+  va_end(vlist);
+  
+  return tot;
+  
+ // panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
