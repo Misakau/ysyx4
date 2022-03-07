c5a28f1efd026fbd0646561c2ba64dcd278c575d
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-08 00:22:40 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:22:40 up  7:08,  1 user,  load average: 0.42, 0.57, 0.59

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -13,14 +13,14 @@ module top(
     );
 
     wire [6:0] op, func7;
-    wire [4:0] rd, rs1;
+    wire [4:0] rd, rs1, rs2;
     wire [2:0] func3;
     wire [31:0] immI;
     wire wen;
     ysyx_220053_IDU my_idu(
       .instr_i(instr_i),
       .op(op),
-      .rd(rd),.rs1(rs1),//rs2,
+      .rd(rd),.rs1(rs1),.rs2(rs2),
       .func3(func3),.func7(func7),
       .immI(immI),//immS,immJ,immB,
       .wen(wen));
