331181eaa4cec2099f00c44bd3187c9797589200
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-07 23:24:42 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:24:42 up  6:10,  1 user,  load average: 0.37, 0.55, 0.56

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -35,9 +35,10 @@ int main(int argc, char**argv, char**env) {
         top->clk = !top->clk;
         //top->rst = rand()&1;
         top->instr_i = pmem_read(top->pc);
+        printf("cnt = %d,clk = %d, rst = %d, pc = %lx, instr = %x\n", cnt, top->clk, top->rst, top->pc, top->instr_i);
         top->eval();
         cnt ++;
-        printf("cnt = %d,clk = %d, rst = %d, pc = %lx, instr = %x\n", cnt, top->clk, top->rst, top->pc, top->instr_i);
+        
     }
     delete top;
     delete contextp;
