b838f292cb99fc5d78de8433aa5212aea6881e53
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-08 16:24:38 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:24:38 up  5:23,  1 user,  load average: 0.65, 0.60, 0.63

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -3,15 +3,14 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  CPU_state dut_r;
-  ref_difftest_regcpy(&dut_r, DIFFTEST_TO_DUT);
   for(int i = 0; i < 32; i++){
-    Log("gpr[%d]=%lx",i, ref_r->gpr[i]);
+    Log("ref_gpr[%d]=%lx",i, ref_r->gpr[i]);
+    Log("ref_gpr[%d]=%lx",i, cpu.gpr[i]);
   }
-  if(pc == dut_r.pc){
+  if(pc == cpu.pc){
     for(int i = 0; i < 32; i++){
-      if(ref_r->gpr[i] != dut_r.gpr[i]){
-        Log("ref_r->gpr[%d]=%lx, while nemu_r->gpr[%d]=%lx\n",i,ref_r->gpr[i],i,dut_r.gpr[i]);
+      if(ref_r->gpr[i] != cpu.gpr[i]){
+        Log("ref_r->gpr[%d]=%lx, while nemu_r->gpr[%d]=%lx\n",i,ref_r->gpr[i],i,cpu.gpr[i]);
         return false;
       }
         
@@ -20,7 +19,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
       
     return true;
   }
-  Log("pc=%lx, while nemu_pc=%lx\n", pc, dut_r.pc);
+  Log("pc=%lx, while nemu_pc=%lx\n", pc, cpu.pc);
   return true;
 }
 
