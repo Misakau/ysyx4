2303f9d7e7cdf13ec70fa6da1144aad9d5341b52
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-08 16:03:54 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:03:54 up  5:02,  1 user,  load average: 0.62, 0.71, 0.63

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -4,29 +4,14 @@
 #include <memory/paddr.h>
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  /*if(direction == DIFFTEST_TO_DUT){
-    memcpy(buf, guest_to_host(addr), n);
-  } 
-  else if(direction == DIFFTEST_TO_REF){
-    paddr_write(addr, n, paddr_read(host_to_guest(buf), n));
-  }
-  else */
-  Log("hh");
   assert(0);
 }
 
 void difftest_regcpy(void *dut, bool direction) {
-  /*if(direction == DIFFTEST_TO_DUT){
-    memcpy(dut, &cpu, DIFFTEST_REG_SIZE);
-  } 
-  else if(direction == DIFFTEST_TO_REF){
-    memcpy(&cpu, dut, DIFFTEST_REG_SIZE);
-  }
-  else */assert(0);
+  assert(0);
 }
 
 void difftest_exec(uint64_t n) {
-  //cpu_exec(n);
   assert(0);
 }
 
--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -3,10 +3,14 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  
   if(pc == cpu.pc){
-    for(int i = 0; i < 32; i++)
+    for(int i = 0; i < 32; i++){
       if(ref_r->gpr[i] != cpu.gpr[i])
         return false;
+      Log("hh\n");
+    }
+      
     return true;
   }
   return false;
