d209e6971e24d5f7ba6bc2ed4352185b69189e70
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-08 14:35:45 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:35:45 up  3:34,  1 user,  load average: 0.57, 0.69, 0.64

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -13,11 +13,17 @@ uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
+#ifdef CONFIG_MTRACE
+  log_write("<mtrace>: read %d bytes from %lx\n", len, addr);
+#endif
   word_t ret = host_read(guest_to_host(addr), len);
   return ret;
 }
 
 static void pmem_write(paddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE
+  log_write("<mtrace>: write %d bytes data %lx from %lx\n", len, data, addr);
+#endif
   host_write(guest_to_host(addr), len, data);
 }
 
