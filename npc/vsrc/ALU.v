0fc9d3a76293cd35724211f6d72730031eaa6c63
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-11 14:56:30 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:56:30 up  4:43,  1 user,  load average: 0.52, 0.62, 0.58

--- npc/vsrc/ALU.v
+++ npc/vsrc/ALU.v
@@ -12,9 +12,9 @@ module ysyx_220053_ALU(
     ysyx_220053_Adder64 adder(.result(res0),.x(inputa),.y(inputb),.sub(sub));
     assign res1 = inputb;
     always@(*) begin
-        case(ALUOp) begin
+        case(ALUOp)
          4'b1111: result = res1;
          default: result = res0;
-        end
+        endcase
     end
 endmodule
