83602588f5f1dc562e2f03066acb2ffd20614b08
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-11 13:45:02 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:45:02 up  3:32,  1 user,  load average: 0.73, 0.74, 0.68

--- npc/vsrc/IDU.v
+++ npc/vsrc/IDU.v
@@ -29,33 +29,3 @@ module ysyx_220053_IDU(
     
 
 endmodule
-
-module ysyx_220053_controler(
-    input [31:0] instr_i,
-    input [6:0] op,
-    input [2:0] func3,
-    output reg [2:0] ExtOp,
-    output reg wen
-);
-    always @(*) begin
-        case(op)
-            7'b0010011:
-                begin
-                    //wen = 1;
-                    case(func3)
-                        3'b000: begin ExtOp = 0; wen = 1; end
-                        default: $display("no");
-                    endcase
-                end
-             7'b1110011:
-             	begin
-             		case(instr_i[31:20])
-             			1: begin ExtOp = 0; wen = 0; c_trap(1); end
-             			default: $display("no");
-             		endcase
-             	end
-            default: $display("no");
-        endcase
-    end
-
-endmodule
