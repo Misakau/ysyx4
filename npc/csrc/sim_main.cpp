de2de447917ce5b15962e3d559ee643eaa318700
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-12 16:02:51 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:02:51 up  3:22,  1 user,  load average: 0.30, 0.38, 0.38

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -32,8 +32,8 @@ int main(int argc, char**argv, char**env) {
     contextp->timeInc(1); 
     top->eval();
     //printf("Now_pc = %016lx\n",top->pc);
+    top->clk = 0;
     top->rst = 0;
-    top->instr_i = pimem_read(top->pc);
     int cnt = 0;
     IMEM[0] = 0x7ff00093;// addi x0,x1,1
     IMEM[1] = 0x0ffff097;// auipc x1,0x0ffff
@@ -44,11 +44,10 @@ int main(int argc, char**argv, char**env) {
     while (!is_done && !contextp->gotFinish()) { 
         contextp->timeInc(1); 
         top->clk = !top->clk;
-        
+        top->instr_i = pimem_read(top->pc);
         if(EXIT){printf("ASSERT!\n"); break;}
         printf("Next status: clk = %d, rst = %d, pc = %016lx, instr = %08x\n", top->clk, top->rst, top->pc, top->instr_i);
         top->eval();
-        top->instr_i = pimem_read(top->pc);
         cnt ++;
     }
     delete top;
