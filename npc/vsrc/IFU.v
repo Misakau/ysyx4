81e318dad0a89190c87e60ee175c972aa0648e30
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-12 15:43:49 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:43:49 up  3:03,  1 user,  load average: 0.30, 0.47, 0.39

--- npc/vsrc/IFU.v
+++ npc/vsrc/IFU.v
@@ -6,12 +6,12 @@ module ysyx_220053_IFU(
     input [63:0] dnpc,
     output [63:0] pc
 );
-    wire [63:0] now_pc;
-    //assign snpc = now_pc + 4;
+    wire [63:0] now_pc, snpc;
+    assign snpc = now_pc + 4;
     assign pc = now_pc;
     always@(*)begin
         $display("IN.V: now_pc=%x, dnpc=%x",now_pc, dnpc);
     end
-    ysyx_220053_Reg #(64, 64'h80000000) PC(.clk(~clk), .rst(rst), dnpc, now_pc, 1'b1);
+    ysyx_220053_Reg #(64, 64'h80000000) PC(.clk(~clk), .rst(rst), snpc, now_pc, 1'b1);
 
 endmodule
