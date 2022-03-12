be11b3641a25d6c7e16737106dd0a69b70da23ce
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-12 14:59:03 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:59:03 up  2:18,  1 user,  load average: 0.27, 0.34, 0.29

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -27,7 +27,9 @@ int main(int argc, char**argv, char**env) {
     top->eval();
     top->clk = 1;
     top->eval();
-   // top->rst = 0;
+    top->clk = 0;
+    top->eval();
+    top->rst = 0;
     int cnt = 0;
     IMEM[0] = 0x7ff00093;// addi x0,x1,1
     IMEM[1] = 0x0ffff097;// auipc x1,0x0ffff
