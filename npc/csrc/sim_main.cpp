928142d81cd7fb293472688662aa27c4ed54460e
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-12 15:22:37 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:22:37 up  2:42,  1 user,  load average: 0.40, 0.26, 0.23

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -22,9 +22,9 @@ int main(int argc, char**argv, char**env) {
     
     Vtop*top = new Vtop{contextp};
     //reset the pc
-    top->clk = 0;
-    top->eval();
     top->clk = 1;
+    top->eval();
+    top->clk = 0;
     top->rst = 1;
     top->eval();
     top->rst = 0;
