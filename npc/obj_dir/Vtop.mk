56561e619fe5109e006567f6f88fd09201c343dc
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-15 13:32:08 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:32:08 up  5:12,  1 user,  load average: 1.69, 1.09, 0.86

--- npc/obj_dir/Vtop.mk
+++ npc/obj_dir/Vtop.mk
@@ -39,7 +39,7 @@ VM_USER_CFLAGS = \
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
 	-lreadline \
-	csrc/libnpc_sdb.a \
+	./libcsrc/libnpc_sdb.a \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
