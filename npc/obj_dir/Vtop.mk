a8ac04675f06c488e58b9a37442ed8b9f4bedb98
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-15 13:39:50 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:39:50 up  5:19,  1 user,  load average: 0.83, 1.67, 1.45

--- npc/obj_dir/Vtop.mk
+++ npc/obj_dir/Vtop.mk
@@ -39,10 +39,10 @@ VM_USER_CFLAGS = \
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
 	-lreadline \
-	/home/wang/ysyx-workbench/npc/csrc/libnpc_sdb.a \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
+	npc_sdb \
 	sim_main \
 
 # User .cpp directories (from .cpp's on Verilator command line)
@@ -59,6 +59,8 @@ include $(VERILATOR_ROOT)/include/verilated.mk
 ### Executable rules... (from --exe)
 VPATH += $(VM_USER_DIR)
 
+npc_sdb.o: csrc/npc_sdb.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
 sim_main.o: csrc/sim_main.cpp
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
 
