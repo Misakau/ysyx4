91f3bccdff62c47faf039a6b38de16cc1db480e8
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-17 20:44:31 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:44:31 up  3:00,  1 user,  load average: 0.68, 0.48, 0.50

--- npc/vsrc/templete.v
+++ npc/vsrc/templete.v
@@ -118,8 +118,8 @@ module ysyx_220053_RegisterFile #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
   input wen
 );
   reg [DATA_WIDTH-1:0] rf [(1 << ADDR_WIDTH)-1:0];
-  assign  radata = (raaddr == 5'b00000) ? 0 : rf[raaddr];
-  assign  rbdata = (rbaddr == 5'b00000) ? 0 : rf[rbaddr];
+  assign  radata = (raaddr == 0) ? 0 : rf[raaddr];
+  assign  rbdata = (rbaddr == 0) ? 0 : rf[rbaddr];
   always @(posedge clk) begin
     if (wen) rf[waddr] <= wdata;
   end
