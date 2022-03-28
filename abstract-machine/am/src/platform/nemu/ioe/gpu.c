3433097b9db88ea6333a94e4f5814c63cffee38c
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-29 00:23:12 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:23:12 up 54 min,  1 user,  load average: 0.38, 0.39, 0.35

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,18 +3,33 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+#define N 32
+
 void __am_gpu_init() {
+  int i;
+  int w = io_read(AM_GPU_CONFIG).width / N;  // TODO: get the correct width
+  int h = io_read(AM_GPU_CONFIG).height / N;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = inw(VGACTL_ADDR + 2), .height = inw(VGACTL_ADDR),
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int h = ctl->h, w = ctl->w, x = ctl->x, y = ctl->y;
+  uint32_t *pixels = (uint32_t *)ctl->pixels;
+  for(int i = 0; i < h; i++){
+    for(int j = 0; j < w; j++){
+      outl(FB_ADDR + ((i + y) * w + j + x) * 4, pixels[i * w + j]);
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
