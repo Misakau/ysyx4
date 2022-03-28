ac85bcd2811e97e1d7410fa2cb9576901be67920
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-29 00:53:40 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:53:40 up  1:25,  1 user,  load average: 0.71, 0.72, 0.61

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -39,8 +39,9 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+  void *ptr = (void *)malloc_addr;
   malloc_addr += size;
-  return (void *)malloc_addr;
+  return ptr;
   //panic("Not implemented");
 #endif
   return NULL;
