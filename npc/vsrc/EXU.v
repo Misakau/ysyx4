e0911ca0d097c6e6d0b1d3987ff960c043e61168
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 19:50:27 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:50:27 up  1:27,  1 user,  load average: 0.61, 0.55, 0.55

--- npc/vsrc/EXU.v
+++ npc/vsrc/EXU.v
@@ -45,7 +45,7 @@ module ysyx_220053_EXU(
     assign CsrId = (Ecall == 0) ? imm[11:0] : 12'h342;//ecall mcause
     wire [63:0] datain, mtvec;
     ysyx_220053_CSR csrfile( .clk(clk), .Csrwen(Csrwen), .CsrOp(CsrOp), .CsrId(CsrId), .datain(busa),
-                             .mepc_o(mepc), .csrres(csrres), .mtvec_o(mtvec), .Ecall(Ecall), .mepc_in(pc));
+                             .mepc_o(mepc), .csrres(csrres), .mtvec_o(mtvec), .Ecall(Ecall), .epc_in(pc));
     
     ysyx_220053_ALU alu64(.inputa(alu_inA), .inputb(alu_inB), .ALUOp(ALUOp), .MulOp(MulOp), .result(res), .zero(zero));
         //busa + immI; //addi
