b4b00d6b608294d6bb92e1a29cab01fb0c1d7f35
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 18:51:36 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:51:36 up 29 min,  1 user,  load average: 0.87, 0.72, 0.54

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -57,7 +57,7 @@ extern "C" void pmem_read(long long raddr, long long *rdata) {
     struct timeval tv;
     gettimeofday(&tv, NULL);
     *rdata = tv.tv_usec;
-    printf("%llx\n",*rdata);
+    //printf("%llx\n",*rdata);
   }
   else{
     long long real_addr = (raddr - AD_BASE) >> 3;
