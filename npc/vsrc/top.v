37867a614c15663f214855a4337f965f26de4d08
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-03 16:49:44 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:49:44 up 33 min,  1 user,  load average: 0.81, 0.71, 0.58

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -348,7 +348,7 @@ module top(
     .wen_o(wb_wen_i),
     .wdata_o(wb_wdata_i),
     .waddr_o(wb_waddr_i),
-    .Ebreak_o(wb_Ebreak_i),
+    .Ebreak_o(wb_Ebreak_i)
     );
     ///////////WB////////////////
     wire is_wen = (~wb_flush) & wb_wen_i;
