44b2f697d7b047fa3543bc860e58124230dfc8f9
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-03 20:43:16 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:43:16 up 19 min,  1 user,  load average: 0.45, 0.32, 0.22

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -326,7 +326,7 @@ static void npc_exec(uint64_t n){
                 if(log_ptr) fprintf(log_ptr, "pc = 0x%016lx, instr = %08x %s\n", sdb_top->pc, instr_now, str);
               }
               if(sdb_top->clk == 1 && sdb_top->wb_commit == 1){
-                print("clk = %x\n",sdb_top->clk);
+                printf("clk = %x\n",sdb_top->clk);
                 if(n != -1) printf("wb_commit: pc = 0x%016lx, instr = %08x\n", sdb_top->wb_pc, sdb_top->wb_instr);
                 //if(log_ptr){fprintf(log_ptr, "pc = 0x%016lx, instr = %08x %s\n", sdb_top->pc, instr_now, str);}
                 if(log_ptr) fprintf(log_ptr, "wb_commit: pc = 0x%016lx, instr = %08x\n", sdb_top->wb_pc, sdb_top->wb_instr);
