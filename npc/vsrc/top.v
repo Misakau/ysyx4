8947bf972b910ace2d5ed480ffecd48b1438472c
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-03 16:28:22 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:28:22 up 12 min,  1 user,  load average: 0.45, 0.35, 0.21

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -313,9 +313,9 @@ module top(
     ///commit a finish instr
     reg wb_valid_r;
     always@(posedge clk) begin
-        if(wb_flush){
+        if(wb_flush)begin
             wb_valid_r <= 1'b0;
-        }
+        end
         else wb_valid_r <= wb_valid_o;
     end
     wire wb_commit = wb_valid_r;
