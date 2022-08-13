34e5b6a2e66f92bb618748104780aba19b86deae
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-13 14:12:40 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:12:40 up  2:47,  1 user,  load average: 0.76, 0.65, 0.47

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -249,7 +249,7 @@ int main(int argc, char**argv, char**env) {
     int cnt = 0;
     
     if(is_batch){
-      #undef ITRACE
+      //#undef ITRACE
       int step = 0;
         NEMU_CPU nemu;
         while (!is_done && !contextp->gotFinish()) { 
