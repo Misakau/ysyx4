12c2215bf81c75446ba9fcab1765413c710618fb
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-14 18:17:37 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:17:37 up  1:19,  1 user,  load average: 1.04, 1.05, 0.80

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -104,7 +104,7 @@ extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
    // printf("real_mask = %lx\n",real_mask);
     assert(real_mask == 0xffffffffull);
     uint32_t *ptr = (uint32_t *)vmem;
-    printf("waddr = %lx, waddr-FB_ADDR = %lx\n",waddr,waddr-FB_ADDR);
+    printf("waddr = %llx, waddr-FB_ADDR = %llx\n",waddr,waddr-FB_ADDR);
     ptr[waddr-FB_ADDR] = wdata;
   }
   else{
