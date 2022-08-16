1dfd5da786c0198b4eebe4cc3ba7572cbec11075
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-16 13:24:00 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:24:00 up  3:06,  1 user,  load average: 0.69, 0.64, 0.50

--- npc/vsrc/Mem.v
+++ npc/vsrc/Mem.v
@@ -79,9 +79,21 @@ module ysyx_220053_Mem(
     always @(*) begin
         //tmp = {{29{1'b0}},raddr[2:0]};
         case(MemOp[1:0])
-            2'b00: dataw = dataout[st + 31 : st];
-            2'b01: datab = dataout[st + 7 : st];
-            2'b10: datah = dataout[st + 15 : st];
+            2'b00: begin
+                for (i = 0; i < 32; i = i + 1) begin
+                    dataw[i] = dataout[st + i];
+                end
+            end
+            2'b01: begin
+                for (i = 0; i < 8; i = i + 1) begin
+                    datab[i] = dataout[st + i];
+                end
+            end
+            2'b10: begin
+                for (i = 0; i < 16; i = i+1) begin
+                    datah[i] = dataout[st + i];
+                end
+            end
             default: datad = dataout;
         endcase
     end
