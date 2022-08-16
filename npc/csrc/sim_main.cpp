d4cecf569c12d7314b87bbeafe41e5fbe3ca7262
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-16 14:45:10 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:45:10 up  4:28,  1 user,  load average: 0.32, 0.34, 0.29

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -80,6 +80,7 @@ extern "C" void pmem_read(long long raddr, long long *rdata) {
     
     //assert(real_addr < MEMSIZE);
     if(raddr < AD_BASE || ((raddr - AD_BASE) >> 3) >= MEMSIZE){
+      printf("raddr = %llx\n",raddr);
       printf("over!!\n");
       //if(START) EXIT = 1;//printf("addrs=%lx\n",raddr); 
       *rdata = 0;
