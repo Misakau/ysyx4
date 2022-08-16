c2769580c5d83a53ac6e78ec008ab0b7124dbda8
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-17 00:58:54 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:58:54 up 14:41,  1 user,  load average: 0.76, 0.64, 0.58

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -19,7 +19,7 @@ void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
 
 void difftest_regcpy(void *dut, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
-    memcpy((CPU_state*)dut, &cpu, DIFFTEST_REG_SIZE);
+    memcpy((CPU_state*)dut, &cpu, sizeof(CPU_state));
   } else {
     assert(0);
   }
