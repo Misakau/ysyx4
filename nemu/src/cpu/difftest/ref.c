0fba775c41ef139604b2e8d4942f9e73a9bb7a2c
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-17 00:52:15 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:52:15 up 14:35,  1 user,  load average: 0.64, 0.63, 0.58

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -27,6 +27,7 @@ void difftest_regcpy(void *dut, bool direction) {
 
 void difftest_exec(uint64_t n) {
   cpu_exec(n);
+  printf("cpu.pc = %lx\n",cpu.pc);
 }
 
 void difftest_raise_intr(word_t NO) {
