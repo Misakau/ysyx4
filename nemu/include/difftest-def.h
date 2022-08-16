a9b16853462f8884cad05dcd273eeb05d9567bf7
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-17 00:59:26 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:59:26 up 14:42,  1 user,  load average: 0.67, 0.63, 0.58

--- nemu/include/difftest-def.h
+++ nemu/include/difftest-def.h
@@ -13,7 +13,7 @@ enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
 #elif defined(CONFIG_ISA_riscv32)
 # define DIFFTEST_REG_SIZE (sizeof(uint32_t) * 33) // GRPs + pc
 #elif defined(CONFIG_ISA_riscv64)
-# define DIFFTEST_REG_SIZE (sizeof(uint64_t) * (33 + 4096)) // GRPs + csr + pc
+# define DIFFTEST_REG_SIZE (sizeof(uint64_t) * 33) // GRPs + pc
 #else
 # error Unsupport ISA
 #endif
