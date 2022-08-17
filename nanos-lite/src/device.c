3ca168022e9ec1ee92b2699bd4c8e71de0b8e793
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-17 16:15:02 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:15:02 up  5:24,  1 user,  load average: 1.20, 0.86, 0.76

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -48,6 +48,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T gpu = io_read(AM_GPU_CONFIG);
   size_t pix = offset >> 2;
+  printf("offset = %d\n",offset);
   int wi = gpu.width;
   int x, y;
   y = pix / wi; x = pix % wi;
