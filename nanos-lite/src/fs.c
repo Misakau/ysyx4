e82269ca273225a1986a18a101b359f624139bce
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-17 13:56:08 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:56:08 up  3:05,  1 user,  load average: 0.75, 0.52, 0.41

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -24,11 +24,13 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
+size_t serial_write(const void *buf, size_t offset, size_t len);
+
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
+  [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
 
@@ -76,13 +78,9 @@ size_t fs_read(int fd, void *buf, size_t len){
 }
 
 size_t fs_write(int fd, const void *buf, size_t len){
-  if(fd < 3){
-    if(fd == 0) return 0;
-    char *str = (char *)buf;
-    for(int i = 0; i < len; i++)
-      putch(str[i]);
-    return len;
-  }
+  if(file_table[fd].write)
+    return file_table[fd].write(buf, 0, len);
+  
   assert(fd >= 3);
   assert(buf);
   size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
