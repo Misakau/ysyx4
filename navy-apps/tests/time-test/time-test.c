9f50dc5226c635266ea692bad8571aff605d7137
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-17 14:23:50 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:23:50 up  3:33,  1 user,  load average: 0.67, 0.63, 0.60

--- navy-apps/tests/time-test/time-test.c
+++ navy-apps/tests/time-test/time-test.c
@@ -1,24 +1,27 @@
 #include <sys/time.h>
 #include <stdio.h>
 #include <assert.h>
-//int NDL_Init(uint32_t flags);
-//uint32_t NDL_GetTicks();
+int NDL_Init(uint32_t flags);
+uint32_t NDL_GetTicks();
 
 int main() {
   time_t lsec = 0;
-  //NDL_Init(0);
+  NDL_Init(0);
+  uint32_t lms = 0; 
   while(1){
-  	struct timeval tv;
+  	/*struct timeval tv;
     assert(gettimeofday(&tv,NULL) == 0);
     time_t sec = tv.tv_sec;
     if(sec > lsec){
       lsec = sec;
       printf("Time is %ld s, tv->us = %ld\n", sec, tv.tv_usec);
     }
-    
-    //uint32_t timeus=NDL_GetTicks();
-    //if(timeus%500000<=10) 
-    //printf("Time is %d\n",timeus); 
+    */
+    uint32_t ms=NDL_GetTicks();
+    if(ms > lms + 5000){
+      lms = ms;
+      printf("Time is %d\n",ms); 
+    } 
   }
   return 0;
 }
