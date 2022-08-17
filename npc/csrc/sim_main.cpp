078ab7f94eda2256968ac1ed1be646f979aad7d0
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-17 14:50:08 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:50:08 up  3:59,  1 user,  load average: 0.48, 0.60, 0.59

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -90,15 +90,13 @@ extern "C" void pmem_read(long long raddr, long long *rdata, char bytes) {
     else{
       long long maddr = (raddr + bytes - 1 - AD_BASE) >> 3;
       long long ret;
-      ret = MEM[real_addr];
-      /*
-      if(maddr == real_addr) 
+      if(maddr == real_addr) ret = MEM[real_addr];
       else{
-          printf("unaligned!\n");
-          printf("raddr = %llx\n",raddr);
-          printf("bytes = %d\n",bytes);
-          printf("real addr = %llx\n",real_addr);
-          printf("maddr = %llx\n",maddr);
+          
+         // printf("raddr = %llx\n",raddr);
+         // printf("bytes = %d\n",bytes);
+         // printf("real addr = %llx\n",real_addr);
+         // printf("maddr = %llx\n",maddr);
           //assert(0);
           assert(maddr == real_addr + 1);
           long long del = (raddr - AD_BASE) - (real_addr << 3);
@@ -110,7 +108,7 @@ extern "C" void pmem_read(long long raddr, long long *rdata, char bytes) {
           long long data2 = MEM[maddr]& mask << (bytes1 * 8);
           ret = data2 | data1;
         
-      }*/
+      }
       *rdata = ret;
     } 
   }
