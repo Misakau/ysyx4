de8b50ade76dc4738b26305396f985884f6e76e2
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-17 15:14:51 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:14:50 up  4:24,  1 user,  load average: 1.45, 1.03, 0.84

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -18,6 +18,7 @@ uint32_t NDL_GetTicks() {
 
 int NDL_PollEvent(char *buf, int len) {
   int fd = open("/dev/events", 0, 0);
+  printf("fd = %d\n",fd);
   int bytes = read(fd, buf, len);
   close(fd);
   if(bytes) return 1;
