1e63168040fbcd4cae43548340deece33f62f4eb
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 10:37:58 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:37:58 up 36 min,  1 user,  load average: 0.45, 0.55, 0.51

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {//////////////???
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
@@ -71,8 +71,10 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
   //printf("%x\n",*(s->pixels));
   //assert(0);
-  if(s->format->BitsPerPixel==32)
+  if(s->format->BitsPerPixel==32){
+    printf("update\n");
     NDL_DrawRect((uint32_t*)(s->pixels), x, y, w, h);//assert(0);
+  }
  	else{
   	int real_w=w,real_h=h; 
  	if(w==0&&h==0){real_w=s->w;real_h=s->h;}
