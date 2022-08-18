b4a6bf7d6aa864400509efb1ed09ffb948fc430a
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 11:46:40 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:46:40 up  1:45,  1 user,  load average: 11.27, 4.04, 1.72

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -12,8 +12,8 @@ Context* __am_irq_handle(Context *c) {
         if(c->GPR1 == -1) ev.event = EVENT_YIELD;
         else if(c->GPR1 >= 0 && c->GPR1 <= 19) ev.event = EVENT_SYSCALL;
         else ev.event = EVENT_ERROR;
-        break;
         c->mepc += 4;
+        break;
       default: ev.event = EVENT_ERROR; break;
     }
 
