e8db1346f8fd0d832628af500ed9f0b06f293a92
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 14:05:07 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:05:07 up  4:03,  1 user,  load average: 0.80, 0.78, 0.83

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,72 +15,43 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
- // yield();
-  int buf_size=strlen(buf);
-  if(len<buf_size) buf_size=len;
-  for(int i=0;i<buf_size;i++) putch(*(((char*)buf)+i));
+  char *str = (char *)buf;
+  for(int i = 0; i < len; i++)
+    putch(str[i]);
   return len;
-   //return 0;
 }
+
 size_t events_read(void *buf, size_t offset, size_t len) {
-  //yield();
+  char *str = buf;
   AM_INPUT_KEYBRD_T ev=io_read(AM_INPUT_KEYBRD);
-  bool isdown=ev.keydown;
-  int code=ev.keycode;
-  if(code==0) return 0;
-  memset(buf,0,sizeof(buf));///actually, sizeof(buf) is only 4 bytes;
-  if(isdown){ 
-    strcat(buf,"kd ");
-  }
+  bool isdown = ev.keydown;
+  int code = ev.keycode;
+  if(code == 0) return 0;
+  str[0] = 0;
+  if(isdown) strcat(buf,"kd ");
   else strcat(buf,"ku ");
   strcat(buf,keyname[code]);
   strcat(buf,"\n");
-  
-  return strlen(buf);
+  int ret = strlen(buf);
+  assert(ret <= len);
+  return ret;
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  AM_GPU_CONFIG_T gpu=io_read(AM_GPU_CONFIG);
-  char* buff=buf;
-  buff[0]='\0';
-  strcat(buff,"WIDTH:");
-  int k=gpu.width,tmpn=0,lbuf=0;char tmp[64];
-  if(k==0) strcat(buff,"0\n");
-  else{
-    while(k){
-      tmp[tmpn++]=k%10+'0';
-      k/=10;
-    }
-    lbuf=strlen(buff);
-    for(int i=tmpn-1;i>=0;i--) buff[lbuf++]=tmp[i];
-    buff[lbuf]='\0';
-    strcat(buff,"\n");
-  }
-  k=gpu.height;tmpn=0;
-  strcat(buff,"HEIGHT:");
-  if(k==0) strcat(buff,"0\n");
-  else{
-    while(k){
-      tmp[tmpn++]=k%10+'0';
-      k/=10;
-    }
-    lbuf=strlen(buff);
-    for(int i=tmpn-1;i>=0;i--) buff[lbuf++]=tmp[i];
-    buff[lbuf]='\0';
-    strcat(buff,"\n");
-  }
-  //Log("%p",buf);
-  return strlen(buff);
+  AM_GPU_CONFIG_T gpu = io_read(AM_GPU_CONFIG);
+  sprintf(buf, "WIDTH:%d\nHEIGHT:%d\n", gpu.width, gpu.height);
+  int ret = strlen(buf);
+  assert(ret <= len);
+  return ret;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
- // yield();
-  AM_GPU_CONFIG_T gpu=io_read(AM_GPU_CONFIG);
-  int wi=gpu.width;
-  int x,y;
-  y=offset/4/wi; x=offset/4%wi;
- // printf("x=%d,y=%d\n",x,y);
-  io_write(AM_GPU_FBDRAW, x, y, (uint32_t*)buf, len>>2, 1, true);
+  AM_GPU_CONFIG_T gpu = io_read(AM_GPU_CONFIG);
+  size_t pix = offset >> 2;
+  int wi = gpu.width;
+  int x, y;
+  y = pix / wi; x = pix % wi;
+  io_write(AM_GPU_FBDRAW, x, y, (uint32_t*)buf, len >> 2, 1, true);
   return len;
 }
 
