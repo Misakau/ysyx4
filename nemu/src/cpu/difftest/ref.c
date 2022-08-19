253c8ea2e0f3a2ba971e9fbe1895e19d97033a15
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-19 13:09:04 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:09:04 up  3:53,  1 user,  load average: 0.57, 0.63, 0.63

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -21,7 +21,7 @@ void difftest_regcpy(void *dut, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
     memcpy((CPU_state*)dut, &cpu, DIFFTEST_REG_SIZE);
   } else {
-    memcpy(&cpu, (CPU_state*)dut, DIFFTEST_REG_SIZE);
+    memcpy(&cpu, (CPU_state*)dut, DIFFTEST_REG_SIZE - 1);
   }
 }
 
