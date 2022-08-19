d27546e9cd0022a4df8673bcc49175e378b90bdc
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-19 13:42:07 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:42:07 up  4:26,  1 user,  load average: 0.41, 0.53, 0.62

--- npc/vsrc/our.v
+++ npc/vsrc/our.v
@@ -1,3 +1,9 @@
 module our;
-initial begin $display("hello world"); end
+wire [63:0] a;
+wire [63:0] b;
+wire [63:0] r;
+assign a = 64'h8000000000000000;
+assign b = 64'ha;
+assign r = a % b;
+initial begin $display("hello world"); $display(r); end
 endmodule
