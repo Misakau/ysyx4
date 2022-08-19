1d75d7f8ff568c6fc1a1f0cb06f00ef5bfc427ff
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-19 14:36:44 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:36:44 up  5:21,  1 user,  load average: 0.60, 0.50, 0.64

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,7 +22,7 @@ void hello_fun(void *arg) {
 void naive_uload(PCB *pcb, const char *filename);
 void init_proc() {
   switch_boot_pcb();
-  naive_uload(NULL,"/bin/menu");
+  naive_uload(NULL,"/bin/nterm");
   Log("Initializing processes...");
 
   // load program here
