5b848dc41bb38d03154de1c5d9197c74c9664fcf
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-19 12:30:43 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:30:43 up  3:15,  1 user,  load average: 1.10, 0.92, 0.80

--- npc/csrc/keyboard.cpp
+++ npc/csrc/keyboard.cpp
@@ -36,6 +36,7 @@ static int key_queue[KEY_QUEUE_LEN] = {};
 static int key_f = 0, key_r = 0;
 
 static void key_enqueue(uint32_t am_scancode) {
+  printf("am_scancode = %d\n",am_scancode);
   key_queue[key_r] = am_scancode;
   key_r = (key_r + 1) % KEY_QUEUE_LEN;
   if(key_r == key_f){
