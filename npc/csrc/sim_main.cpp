2b27c14267c20ffb0b1f4b0968baee3d6803a63b
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-19 13:13:05 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:13:05 up  3:57,  1 user,  load average: 0.66, 0.68, 0.65

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -85,6 +85,7 @@ extern "C" void pmem_read(long long raddr, long long *rdata, char bytes) {
   }
   else if(raddr == KBD_ADDR){
     rd_dev = 1;
+    printf("hh\n");
     assert(i8042_data_port_base);
     //printf("i8042_data_port_base[0] = %x\n",i8042_data_port_base[0]);
     i8042_data_io_handler(0, 4, false);
@@ -409,7 +410,6 @@ int main(int argc, char**argv, char**env) {
                 difftest_exec(1);
                 difftest_regcpy(&nemu, 1);
                 if(rd_dev == true){
-                  printf("hh\n");
                   for(int i = 1; i < 32; i++){
                     nemu.gpr[i] = cpu_gpr[i];
                   }
