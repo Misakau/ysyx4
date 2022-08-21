9b695d5b42136002bce0e4b516aad3905080d547
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-21 14:10:19 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:10:19 up 5 min,  1 user,  load average: 0.33, 0.23, 0.11

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -248,10 +248,10 @@ void set_batch_mode(){
 static uint64_t tot_instr = 0;
 static void sdb_mainloop();
 static bool is_diff = false;
-static char pathi[] = "/home/wang/ysyx-workbench/nanos-lite/build/nanos-lite-riscv64-npc.bin";
+static char pathi[] = "/home/wang/ysyx-workbench/am-kernel/test/cpu-test/build/div-riscv64-npc.bin";
 //static char lgp[] = "/home/wang/log1.txt";
 //";pathi;//
-static char* image_file = NULL;
+static char* image_file = pathi;
 static char* log_file = NULL;
 //static FILE* log_ptr = NULL;
 static int npc_parse_args(int argc, char *argv[]) {
