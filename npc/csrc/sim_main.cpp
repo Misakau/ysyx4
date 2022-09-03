6a28963e46b1b5c6b682888516d0494687b6dadb
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-03 19:48:35 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:48:35 up  2:01,  1 user,  load average: 0.58, 0.27, 0.24

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -586,7 +586,7 @@ static void npc_exec(uint64_t n){
               sdb_top->d_rw_ready_i = 1;
               dmem_ls = i;
             }*/
-            if(i == 530) printf("rw_req = %d, rw_valid_o = %x, rw_addr_o = %lx",sdb_top->rw_req_o, sdb_top->rw_valid_o,sdb_top->rw_addr_o);
+            if(i == 530) printf("i == 530, rw_req = %d, rw_valid_o = %x, rw_addr_o = %lx\n",sdb_top->rw_req_o, sdb_top->rw_valid_o,sdb_top->rw_addr_o);
 
             if(sdb_top->clk == 1 && sdb_top->rw_valid_o == 1){
               long long midx = (sdb_top->rw_addr_o - AD_BASE) >> 3;
