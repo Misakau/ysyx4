14d654af2eafabceeef78eb4cc67e9d8e3ad2f34
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-03 19:52:01 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:52:01 up  2:04,  1 user,  load average: 0.40, 0.36, 0.28

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -608,7 +608,7 @@ static void npc_exec(uint64_t n){
             sdb_top->eval();
             //if(sdb_top->clk == 1 && i == imem_ls + 2) sdb_top->i_rw_ready_i = 0;
             //if(sdb_top->clk == 1 && i == dmem_ls + 2) sdb_top->d_rw_ready_i = 0;
-            if(sdb_top->clk == 1 && i == mem_ls + 1) sdb_top->rw_ready_i = 0;
+            if(sdb_top->clk == 1 && i == mem_ls + 2) sdb_top->rw_ready_i = 0;
             //printf("i_rw_valid_o = %x, i_rw_addr_o = %lx, i_rw_req_o = %x\n",sdb_top->i_rw_valid_o,sdb_top->i_rw_addr_o, sdb_top->i_rw_req_o);
             //NPC_EXIT = 1;break;
             if(sdb_top->clk == 0 && sdb_top->wb_commit == 1){
