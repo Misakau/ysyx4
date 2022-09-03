11bfb42006cd710651323f690f4bc7380895bfcd
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-03 20:48:11 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:48:11 up  3:00,  1 user,  load average: 0.59, 0.41, 0.35

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -434,7 +434,7 @@ int main(int argc, char**argv, char**env) {
                 MEM[midx] = top->rw_w_data_o[0] | (uint64_t)top->rw_w_data_o[1] << 32;
                 MEM[midx + 1] = top->rw_w_data_o[2] | (uint64_t)top->rw_w_data_o[3] << 32;
               }
-              if (top->rw_ready_i == 0) mem_ls = i;
+              if (top->rw_ready_i == 0) mem_ls = step;
               top->rw_ready_i = 1;
             }
             top->eval();
