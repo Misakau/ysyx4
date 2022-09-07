2b3d7ade7dbfd33a6508017ab2c8636ab0a78ec4
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-07 15:42:18 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:42:18 up 31 min,  1 user,  load average: 0.75, 0.58, 0.53

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -702,7 +702,7 @@ static void npc_exec(uint64_t n){
             //printf("i = %ld, rw_req = %d, rw_valid_o = %x, rw_addr_o = %lx, d_rw_ready = %d, rw_ready_i = %d\n",i, sdb_top->rw_req_o, sdb_top->rw_valid_o,sdb_top->rw_addr_o, sdb_top->d_rw_ready,sdb_top->rw_ready_i);
             */
             mem_sig.update_input(mem_ref);
-            
+            printf("axi_ar_addr_o = %llx\n",sdb_top->axi_ar_addr_o);
             sdb_top->eval();
             //printf("i = %ld, rw_req = %d, rw_valid_o = %x, rw_addr_o = %lx, d_rw_ready = %d, rw_ready_i = %d\n",i, sdb_top->rw_req_o, sdb_top->rw_valid_o,sdb_top->rw_addr_o, sdb_top->d_rw_ready,sdb_top->rw_ready_i);
             
