6aae9b60377862d84b0df7fb9d57fdc4e959523f
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-23 13:45:57 +0800

    >  sim RTL ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:45:57 up  3:36,  1 user,  load average: 1.08, 0.87, 0.76

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -437,7 +437,7 @@ static void npc_exec(uint64_t n){
             //printf("Next status: clk = %d, rst = %d, pc = %016lx, instr = %08x\n", sdb_top->clk, sdb_top->rst, sdb_top->pc, sdb_top->instr);
             #ifdef ITRACE
               //if(instr_now == 0) return;
-              char str[128];disassemble(str, 127, sdb_top->pc, (uint8_t*)&instr_now, 4);
+              char str[128];disassemble(str, 127, sdb_top->pc, (uint8_t*)&(sdb_top->wb_instr), 4);
               if(sdb_top->clk == 0){//instr to be fetch
                 if(n != -1) printf("pc = 0x%016lx, instr = %08x %s\n", sdb_top->pc, instr_now, str);
                 //if(log_ptr){fprintf(log_ptr, "pc = 0x%016lx, instr = %08x %s\n", sdb_top->pc, instr_now, str);}
