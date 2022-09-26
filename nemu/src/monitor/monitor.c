3c080bbd17daf3721e8e535c6e550ed7daf22006
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-26 18:54:14 +0800

    >  compile NEMU ysyx_220053 汪洵 Linux wang-Lenovo-XiaoXinAir-14ITL-2021 5.11.0-31-generic #33-Ubuntu SMP Wed Aug 11 13:19:04 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:54:14 up  2:47,  1 user,  load average: 0.29, 0.36, 0.41

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -85,8 +85,10 @@ static void load_elf() {
       for(Elf64_Half i = 0; i < nsym; i++){
         Elf64_Sym symen;
         assert(fread(&symen, symtab.sh_entsize, 1, fp) == 1);
-        printf("symen.st_info = %u\n",(uint32_t)symen.st_info);
+        
         if(ELF64_ST_TYPE(symen.st_info) == STT_FUNC){
+          printf("symen.st_info = %u\n",(uint32_t)symen.st_info);
+          printf("symen.st_shndx = %u\n",(uint32_t)symen.st_shndx);
           strcpy(funcs[tot_func].name,&(strtab_buf[symen.st_shndx]));
           funcs[tot_func].staddr = symen.st_value;
           funcs[tot_func].edaddr = symen.st_value + symen.st_size;
